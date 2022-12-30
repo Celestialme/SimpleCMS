87d70fd823628e130b98a798aaa12ab0e37450ee
Rar9 <RKroells@web.de>
2022-12-30 13:44:35 +0100

    Update Alerts

--- src/components/Alerts.svelte
+++ src/components/Alerts.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-	import { Alert, Toast, Modal, Progressbar, CloseButton } from 'flowbite-svelte';
 	import { fly } from 'svelte/transition';
+
+	// Skeleton
+	import { Alert } from '@skeletonlabs/skeleton';
+	import { ProgressBar } from '@skeletonlabs/skeleton';
+	import { Toast, toastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
+	import { Modal, modalStore } from '@skeletonlabs/skeleton';
+	import type { ModalSettings, ModalComponent } from '@skeletonlabs/skeleton';
+
 	// Icons from https://icon-sets.iconify.design/
 	import Icon from '@iconify/svelte';
 
@@ -10,59 +18,58 @@
 	let progress = '10%';
 	let alertTitle = 'SimpleCMS';
 	let defaultModal = false;
+
+	function triggerToast(): void {
+		const t: ToastSettings = {
+			message: '👋 Hello and welcome to Skeleton.'
+			// Optional: Presets for primary | secondary | tertiary | warning
+			//preset: 'primary',
+			// Optional: The auto-hide settings
+			//autohide: true,
+			//timeout: 5000,
+			// Optional: Adds a custom action button
+			//action: {
+			//	label: 'Greeting',
+			//	response: () => alert('Hello, Skeleton')
+			//}
+		};
+		toastStore.trigger(t);
+	}
 </script>
 
 {#if alertMessage}
 	{#if alertType == Alert}
-		<!-- <div
-			class="absolute bottom-1 right-2 mb-4 flex  bg-gradient-to-br from-lime-600 via-lime-500 to-lime-300 p-4 shadow-xl"
-			role="alert"
-		>
-			<Icon icon={alertIcon} width="24" color="white" />
-			<div class="ml-3 text-lg font-medium text-white">
-				{alertMessage}
-			</div>
-
-			<CloseButton on:click={() => (alertMessage = false)} />
-		</div>
-		
-		<div class="absolute -mt-4 h-1.5 w-full bg-gray-200 dark:bg-gray-800">
-			<div class="h-1.5 bg-gray-900 dark:bg-green-500" style="width: {progress}" />
-		</div> -->
 		<Alert
-			dismissable
-			shadow
-			accent
-			color="white"
 			transition={fly}
 			params={{ x: 200 }}
 			class="absolute bottom-2 right-3 z-20 my-2 bg-gradient-to-br  from-lime-700 via-lime-600 to-lime-500 text-white shadow-xl"
 		>
-			<svelte:fragment slot="icon">
+			<button on:click={() => (alertMessage = '')} class="btn absolute -top-2 right-0">X</button>
+			<svelte:fragment slot="lead">
 				<Icon icon={alertIcon} width="24" color="white" />
 			</svelte:fragment>
 			{alertMessage}
 
-			<Progressbar color="green" progress="50" size="h-2" slot="extra" class="mt-2 " />
+			<ProgressBar value={50} max={100} class="mt-3" />
 		</Alert>
 	{:else if alertType == Toast}
-		<Toast
+		<Toast />
+		<!-- <Toast
 			transition={fly}
 			params={{ x: 200 }}
-			color="green"
 			class="absolute bottom-2 right-2  z-20 my-2"
 		>
 			<svelte:fragment slot="icon">
 				<Icon icon={alertIcon} width="24" />
 			</svelte:fragment>
 			{alertMessage}
-		</Toast>
+		</Toast> -->
 	{:else if alertType == Modal}
 		<Modal title={alertTitle} bind:open={defaultModal} autoclose>
 			{alertMessage}
 			<svelte:fragment slot="footer">
-				<Button on:click={() => alert('Handle "success"')}>I accept</Button>
-				<Button color="alternative">Decline</Button>
+				<button on:click={() => alert('Handle "success"')}>I accept</button>
+				<button color="alternative">Decline</button>
 			</svelte:fragment>
 		</Modal>
 	{/if}
