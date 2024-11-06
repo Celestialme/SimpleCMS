4fa8063c95f69513f1ab71ae8cb6bc5d9e85f71e
celestialme <gigakhutiashvili@gmail.com>
2024-11-06 17:27:24 +0400

    convert richtext

--- src/components/widgets/RichText/RichText.svelte
+++ src/components/widgets/RichText/RichText.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { self } from 'svelte/legacy';
 	import publicConfig from '@root/config/public';
 	import Input from '@src/components/system/inputs/Input.svelte';
 	import DropDown from './components/DropDown.svelte';
 	import ColorSelector from './components/ColorSelector.svelte';
-	import { onMount, onDestroy, tick } from 'svelte';
+	import { onMount, onDestroy, tick, untrack } from 'svelte';
 	import { Editor, Extension } from '@tiptap/core';
 	import StarterKit from '@tiptap/starter-kit';
 	import Link from '@tiptap/extension-link';
@@ -28,27 +29,43 @@
 	import VideoDialog from './components/VideoDialog.svelte';
 	import { Transaction } from '@tiptap/pm/state';
 	import { entryData, mode } from '@src/stores/store.svelte';
-	export let field: FieldType;
-	export const WidgetData = async () => ({ images, data: _data });
-	let fieldName = getFieldName(field);
-	let element;
-	let editor: Editor;
-	let showImageDialog = false;
-	let showVideoDialog = false;
-	let images = {};
-	let active_dropDown = '';
-	export let value = entryData.value[fieldName] || { content: {}, header: {} };
-	let _data = mode.value == 'create' ? { content: {}, header: {} } : value;
-	$: _language = field?.translated ? $contentLanguage : publicConfig.DEFAULT_CONTENT_LANGUAGE;
+
+	let element = $state() as HTMLElement;
+	let editor = $state() as () => Editor;
+	let showImageDialog = $state(false);
+	let showVideoDialog = $state(false);
+	let images = $state({});
+	let active_dropDown = $state('');
+	interface Props {
+		field: FieldType;
+		value?: any;
+		WidgetData?: any;
+	}
+
+	let {
+		field,
+		value = entryData.value[getFieldName(field)] || { content: {}, header: {} },
+		WidgetData = $bindable()
+	}: Props = $props();
+	WidgetData = async () => ({ images, data: _data });
+	let _data = $state(mode.value == 'create' ? { content: {}, header: {} } : value);
+	let _language = $derived(
+		field?.translated ? $contentLanguage : publicConfig.DEFAULT_CONTENT_LANGUAGE
+	);
+	// svelte-ignore state_referenced_locally
 	let previous_language = _language;
 	contentLanguage.subscribe(async (val) => {
-		await tick();
-		editor && editor.commands.setContent(_data.content[val] || '');
+		editor && editor().commands.setContent(_data.content[val] || '');
 	});
-	$: updateTranslationProgress(_data.content, field);
+	$effect(() => {
+		untrack(() => {
+			updateTranslationProgress(_data.content, field);
+		});
+		_data.content[_language];
+	});
 	let deb = debounce(200);
 	onMount(() => {
-		editor = new Editor({
+		let _editor = new Editor({
 			parseOptions: { preserveWhitespace: 'full' },
 			element: element,
 			extensions: [
@@ -80,23 +97,25 @@
 					? _data.content[_language]
 					: value.content[_language] || '',
 
-			onTransaction: ({ transaction }) => {
-				// force re-render so `editor.isActive` works as expected
+			onTransaction: async ({ transaction }) => {
+				// force re-render so `editor().isActive` works as expected
 				active_dropDown = '';
 				if (previous_language == _language) {
 					handleImageDeletes(transaction);
 				}
 				previous_language = _language;
-				editor = editor;
+				let _editor = editor();
+				editor = () => _editor;
 				deb(() => {
-					let content = editor.getHTML();
+					let content = editor().getHTML();
 					content == '<p></p>' && (content = '');
 					_data.content[_language] = content;
 				});
 			}
 		});
+		editor = () => _editor;
 		tick().then(() => {
-			editor.commands.focus('start');
+			editor().commands.focus('start');
 		});
 	});
 	function handleImageDeletes(transaction: Transaction) {
@@ -137,172 +156,181 @@
 		}
 	}
 	onDestroy(() => {
-		if (editor) {
-			editor.destroy();
+		if (editor()) {
+			editor().destroy();
 		}
 	});
-	let textTypes: ComponentProps<DropDown>['items'];
-	let fonts: ComponentProps<DropDown>['items'];
-	let alignText: ComponentProps<DropDown>['items'];
-	let inserts: ComponentProps<DropDown>['items'];
+	let a = $derived(editor && editor());
+	let textTypes: ComponentProps<DropDown>['items'] = $state([]);
+	let fonts: ComponentProps<DropDown>['items'] = $state([]);
+	let alignText: ComponentProps<DropDown>['items'] = $state([]);
+	let inserts: ComponentProps<DropDown>['items'] = $state([]);
+	let floats: ComponentProps<DropDown>['items'] = $state([]);
+	$effect(() => {
+		untrack(() => {
+			textTypes = [
+				{
+					name: 'paragraph',
+					icon: 'icomoon-free:section',
+					active: () => editor().isActive('paragraph'),
+					onClick: () => editor().chain().focus().setParagraph().run()
+				},
+				{
+					name: 'Heading',
+					icon: 'ci:heading-h1',
+					active: () => editor().isActive('heading', { level: 1 }),
+					onClick: () => editor().chain().focus().toggleHeading({ level: 1 }).run()
+				},
+				{
+					name: 'Heading',
+					icon: 'ci:heading-h2',
+					active: () => editor().isActive('heading', { level: 2 }),
+					onClick: () => editor().chain().focus().toggleHeading({ level: 2 }).run()
+				}
+			];
 
-	$: textTypes = [
-		{
-			name: 'paragraph',
-			icon: 'icomoon-free:section',
-			active: () => editor.isActive('paragraph'),
-			onClick: () => editor.chain().focus().setParagraph().run()
-		},
-		{
-			name: 'Heading',
-			icon: 'ci:heading-h1',
-			active: () => editor.isActive('heading', { level: 1 }),
-			onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run()
-		},
-		{
-			name: 'Heading',
-			icon: 'ci:heading-h2',
-			active: () => editor.isActive('heading', { level: 2 }),
-			onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run()
-		}
-	];
+			fonts = [
+				{
+					name: 'Arial',
+					active: () => editor().isActive('textStyle', { fontFamily: 'Arial' }),
+					onClick: () => editor().chain().focus().setFontFamily('Arial').run()
+				},
+				{
+					name: 'Verdana',
+					active: () => editor().isActive('textStyle', { fontFamily: 'Verdana' }),
+					onClick: () => editor().chain().focus().setFontFamily('Verdana').run()
+				},
+				{
+					name: 'Tahoma',
+					active: () => editor().isActive('textStyle', { fontFamily: 'Tahoma' }),
+					onClick: () => editor().chain().focus().setFontFamily('Tahoma').run()
+				},
 
-	$: fonts = [
-		{
-			name: 'Arial',
-			active: () => editor.isActive('textStyle', { fontFamily: 'Arial' }),
-			onClick: () => editor.chain().focus().setFontFamily('Arial').run()
-		},
-		{
-			name: 'Verdana',
-			active: () => editor.isActive('textStyle', { fontFamily: 'Verdana' }),
-			onClick: () => editor.chain().focus().setFontFamily('Verdana').run()
-		},
-		{
-			name: 'Tahoma',
-			active: () => editor.isActive('textStyle', { fontFamily: 'Tahoma' }),
-			onClick: () => editor.chain().focus().setFontFamily('Tahoma').run()
-		},
+				{
+					name: 'Times New Roman',
+					active: () => editor().isActive('textStyle', { fontFamily: 'Times New Roman' }),
+					onClick: () => editor().chain().focus().setFontFamily('Times New Roman').run()
+				},
+				{
+					name: 'Georgia',
+					active: () => editor().isActive('textStyle', { fontFamily: 'Georgia' }),
+					onClick: () => editor().chain().focus().setFontFamily('Georgia').run()
+				},
+				{
+					name: 'Garamond',
+					active: () => editor().isActive('textStyle', { fontFamily: 'Garamond' }),
+					onClick: () => editor().chain().focus().setFontFamily('Garamond').run()
+				}
+			];
 
-		{
-			name: 'Times New Roman',
-			active: () => editor.isActive('textStyle', { fontFamily: 'Times New Roman' }),
-			onClick: () => editor.chain().focus().setFontFamily('Times New Roman').run()
-		},
-		{
-			name: 'Georgia',
-			active: () => editor.isActive('textStyle', { fontFamily: 'Georgia' }),
-			onClick: () => editor.chain().focus().setFontFamily('Georgia').run()
-		},
-		{
-			name: 'Garamond',
-			active: () => editor.isActive('textStyle', { fontFamily: 'Garamond' }),
-			onClick: () => editor.chain().focus().setFontFamily('Garamond').run()
-		}
-	];
-	$: alignText = [
-		{
-			name: 'left',
-			icon: 'fa6-solid:align-left',
-			active: () => editor.isActive({ textAlign: 'left' }),
-			onClick: () => editor.chain().focus().setTextAlign('left').run()
-		},
-		{
-			name: 'right',
-			icon: 'fa6-solid:align-right',
-			active: () => editor.isActive({ textAlign: 'right' }),
-			onClick: () => editor.commands.setTextAlign('right')
-		},
-		{
-			name: 'center',
-			icon: 'fa6-solid:align-center',
-			active: () => editor.isActive({ textAlign: 'center' }),
-			onClick: () => editor.chain().focus().setTextAlign('center').run()
-		},
-		{
-			name: 'justify',
-			icon: 'fa6-solid:align-justify',
-			active: () => editor.isActive({ textAlign: 'justify' }),
-			onClick: () => editor.chain().focus().setTextAlign('justify').run()
-		}
-	];
-	$: inserts = [
-		{
-			name: 'image',
-			icon: 'fa6-solid:image',
-			onClick: () => {
-				showImageDialog = true;
-			},
-			active: () => editor.isActive('image')
-		},
-		{
-			name: 'video',
-			icon: 'fa6-solid:video',
-			onClick: () => {
-				// editor.commands.setYoutubeVideo({
-				// 	src: 'https://www.youtube.com/watch?v=Q2x2KdHtZ_w'
-				// });
-				showVideoDialog = true;
-			},
-			active: () => editor.isActive('video')
-		}
-	];
-	$: floats = [
-		{
-			name: 'wrap left',
-			icon: 'teenyicons:align-left-solid',
-			onClick: () => editor.chain().focus().setImageFloat('left').run(),
-			active: () => false
-		},
-		{
-			name: 'wrap right',
-			icon: 'teenyicons:align-right-solid',
-			onClick: () => editor.chain().focus().setImageFloat('right').run(),
-			active: () => false
-		},
-		{
-			name: 'unwrap',
-			icon: 'mdi:filter-remove',
-			onClick: () => editor.chain().focus().setImageFloat('unset').run(),
-			active: () => false
-		}
-	];
-	let fontSize = 16;
-	$: editor &&
-		(fontSize =
-			editor.getAttributes('textStyle').fontSize ||
-			window
-				.getComputedStyle(
-					window.getSelection()?.focusNode?.parentElement || (element as HTMLElement)
-				)
-				.fontSize.replace('px', ''));
-	let show = (
-		button:
-			| 'textType'
-			| 'font'
-			| 'align'
-			| 'insert'
-			| 'float'
-			| 'color'
-			| 'bold'
-			| 'italic'
-			| 'strike'
-			| 'link'
-			| 'fontSize'
-			| 'description'
-	) => {
-		if (editor?.isActive('image')) {
-			return ['float', 'align', 'description'].includes(button);
-		}
-		if (['description', 'float'].includes(button)) {
-			return false;
-		}
-		return true;
-	};
-	$: {
-		show = show;
+			alignText = [
+				{
+					name: 'left',
+					icon: 'fa6-solid:align-left',
+					active: () => editor().isActive({ textAlign: 'left' }),
+					onClick: () => editor().chain().focus().setTextAlign('left').run()
+				},
+				{
+					name: 'right',
+					icon: 'fa6-solid:align-right',
+					active: () => editor().isActive({ textAlign: 'right' }),
+					onClick: () => editor().commands.setTextAlign('right')
+				},
+				{
+					name: 'center',
+					icon: 'fa6-solid:align-center',
+					active: () => editor().isActive({ textAlign: 'center' }),
+					onClick: () => editor().chain().focus().setTextAlign('center').run()
+				},
+				{
+					name: 'justify',
+					icon: 'fa6-solid:align-justify',
+					active: () => editor().isActive({ textAlign: 'justify' }),
+					onClick: () => editor().chain().focus().setTextAlign('justify').run()
+				}
+			];
+			inserts = [
+				{
+					name: 'image',
+					icon: 'fa6-solid:image',
+					onClick: () => {
+						showImageDialog = true;
+					},
+					active: () => editor().isActive('image')
+				},
+				{
+					name: 'video',
+					icon: 'fa6-solid:video',
+					onClick: () => {
+						// editor().commands.setYoutubeVideo({
+						// 	src: 'https://www.youtube.com/watch?v=Q2x2KdHtZ_w'
+						// });
+						showVideoDialog = true;
+					},
+					active: () => editor().isActive('video')
+				}
+			];
+			floats = [
+				{
+					name: 'wrap left',
+					icon: 'teenyicons:align-left-solid',
+					onClick: () => editor().chain().focus().setImageFloat('left').run(),
+					active: () => false
+				},
+				{
+					name: 'wrap right',
+					icon: 'teenyicons:align-right-solid',
+					onClick: () => editor().chain().focus().setImageFloat('right').run(),
+					active: () => false
+				},
+				{
+					name: 'unwrap',
+					icon: 'mdi:filter-remove',
+					onClick: () => editor().chain().focus().setImageFloat('unset').run(),
+					active: () => false
+				}
+			];
+		});
 		editor;
-	}
+	});
+
+	let fontSize = $state(16);
+	$effect(() => {
+		editor() &&
+			(fontSize =
+				editor().getAttributes('textStyle').fontSize ||
+				window
+					.getComputedStyle(
+						window.getSelection()?.focusNode?.parentElement || (element as HTMLElement)
+					)
+					.fontSize.replace('px', ''));
+	});
+	let show = $state(
+		(
+			button:
+				| 'textType'
+				| 'font'
+				| 'align'
+				| 'insert'
+				| 'float'
+				| 'color'
+				| 'bold'
+				| 'italic'
+				| 'strike'
+				| 'link'
+				| 'fontSize'
+				| 'description'
+		) => {
+			if (editor()?.isActive('image')) {
+				return ['float', 'align', 'description'].includes(button);
+			}
+			if (['description', 'float'].includes(button)) {
+				return false;
+			}
+			return true;
+		}
+	);
+	$inspect(a);
 </script>
 
 <Input
@@ -311,6 +339,7 @@
 	placeholder="Title"
 	inputClass="!w-full mt-2"
 />
+
 <div class="editor">
 	{#if editor}
 		<div class="translate-x-0 z-10 w-full">
@@ -334,56 +363,62 @@
 					key="color"
 					bind:active={active_dropDown}
 					show={show('color')}
-					color={editor.getAttributes('textStyle').color || '#000000'}
-					on:change={(e) => editor.chain().focus().setColor(e.detail).run()}
+					color={editor().getAttributes('textStyle').color || '#000000'}
+					on:change={(e) => editor().chain().focus().setColor(e.detail).run()}
 				/>
 
 				<div class="flex items-center" class:hidden={!show('fontSize')}>
 					<button
-						on:click={() => {
+						aria-label="Decrease Font Size"
+						onclick={() => {
 							fontSize--;
-							editor.chain().focus().setFontSize(fontSize).run();
+							editor().chain().focus().setFontSize(fontSize).run();
 						}}
 					>
-						<iconify-icon icon="ic:twotone-minus" width="20" />
+						<iconify-icon icon="ic:twotone-minus" width="20"></iconify-icon>
 					</button>
 					<input type="text" class="w-[30px] outline-none text-center" bind:value={fontSize} />
 					<button
-						on:click={() => {
+						aria-label="Increase Font Size"
+						onclick={() => {
 							fontSize++;
-							editor.chain().focus().setFontSize(fontSize).run();
+							editor().chain().focus().setFontSize(fontSize).run();
 						}}
 					>
-						<iconify-icon icon="ph:plus-bold" width="20" />
+						<iconify-icon icon="ph:plus-bold" width="20"></iconify-icon>
 					</button>
 				</div>
 				<button
+					aria-label="Bold"
 					class:hidden={!show('bold')}
-					on:click={() => editor.chain().focus().toggleBold().run()}
-					class:active={editor.isActive('bold')}
+					onclick={() => editor().chain().focus().toggleBold().run()}
+					class:active={editor().isActive('bold')}
 				>
-					<iconify-icon icon="bi:type-bold" width="20" />
+					<iconify-icon icon="bi:type-bold" width="20"></iconify-icon>
 				</button>
 				<button
+					aria-label="Italic"
 					class:hidden={!show('italic')}
-					on:click={() => editor.chain().focus().toggleItalic().run()}
-					class:active={editor.isActive('italic')}
+					onclick={() => editor().chain().focus().toggleItalic().run()}
+					class:active={editor().isActive('italic')}
 				>
-					<iconify-icon icon="lucide:italic" width="20" />
+					<iconify-icon icon="lucide:italic" width="20"></iconify-icon>
 				</button>
 				<button
+					aria-label="Strikethrough"
 					class:hidden={!show('strike')}
-					on:click={() => editor.chain().focus().toggleStrike().run()}
-					class:active={editor.isActive('strike')}
+					onclick={() => editor().chain().focus().toggleStrike().run()}
+					class:active={editor().isActive('strike')}
 				>
-					<iconify-icon icon="majesticons:strike-through-line" width="20" />
+					<iconify-icon icon="majesticons:strike-through-line" width="20"></iconify-icon>
 				</button>
 				<button
+					aria-label="Link"
 					class:hidden={!show('link')}
-					on:click={() => editor.chain().focus().toggleLink({ href: 'https://google.com' }).run()}
-					class:active={editor.isActive('link')}
+					onclick={() => editor().chain().focus().toggleLink({ href: 'https://google.com' }).run()}
+					class:active={editor().isActive('link')}
 				>
-					<iconify-icon icon="pajamas:link" width="20" />
+					<iconify-icon icon="pajamas:link" width="20"></iconify-icon>
 				</button>
 				<DropDown
 					key="align"
@@ -412,9 +447,9 @@
 					bind:active={active_dropDown}
 					key="description"
 					show={show('description')}
-					value={editor.getAttributes('image').description}
+					value={editor().getAttributes('image').description}
 					on:submit={(e) => {
-						editor.chain().focus().setImageDescription(e.detail).run();
+						editor().chain().focus().setImageDescription(e.detail).run();
 					}}
 				/>
 				<FileInput
@@ -428,23 +463,23 @@
 							url = URL.createObjectURL(data);
 							let image_id = createRandomID().toString();
 							images[image_id] = data;
-							editor.chain().focus().setImage({ src: url, id: image_id }).run();
+							editor().chain().focus().setImage({ src: url, id: image_id }).run();
 						} else {
 							url = data.original.url;
 
-							editor.chain().focus().setImage({ src: url, storage_image: data._id }).run();
+							editor().chain().focus().setImage({ src: url, storage_image: data._id }).run();
 						}
 					}}
 				/>
-				<VideoDialog bind:show={showVideoDialog} {editor} />
+				<VideoDialog bind:show={showVideoDialog} editor={editor()} />
 			</div>
 		</div>
 	{/if}
 	<div
-		on:pointerdown|self={() => editor.commands.focus('end')}
+		onpointerdown={self(() => editor().commands.focus('end'))}
 		class="text_Area RichText"
 		bind:this={element}
-	/>
+	></div>
 </div>
 
 <style>
