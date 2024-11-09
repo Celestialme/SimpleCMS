de4ebc386e64c908f11b8fe8644095deeb07ebb5
celestialme <gigakhutiashvili@gmail.com>
2024-11-09 15:43:25 +0400

    theme cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { collection } from '@src/stores/store.svelte';
 	import '../app.css';
-	import '../theme.postcss';
 	import 'iconify-icon';
 	let { children } = $props();
 	$inspect(collection());
